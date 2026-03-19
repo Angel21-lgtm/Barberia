817f5869f8e16664f7d367f3ae23c0ba24880eb5
Angel21-lgtm <mc603452@gmail.com>
2026-03-19 15:39:36 -0600

    Últimas mejoras de hoy

--- script/secciones/01-inicio/backEndInicio.js
+++ script/secciones/01-inicio/backEndInicio.js
@@ -9,14 +9,14 @@ const backEndPromociones = [
         imgBoton: "./img/secciones/inicio/promociones/barbero.png",
         dataTextContenedorPadre: "¡Barba y bigote $150!",
         dataTextContenedorHijo: "Lunes",
-        title: "Promoción 1.",
+        title: "Promoción 1",
         href: "#"
     },
     {
         imgBoton: "./img/secciones/inicio/promociones/tijeras.png",
         dataTextContenedorPadre: "¡Mascarilla $50!",
         dataTextContenedorHijo: "Martes",
-        title: "Promoción 2.",
+        title: "Promoción 2",
         href: "#"
     },
 ];
@@ -29,7 +29,7 @@ const innerHTMLPromociones = (contenedor) => {
         contenido += `
             <li data-text="${promocion.dataTextContenedorPadre}"
             class="contenedor-link-1 display-flex align-items-center justify-content-center">
-                <a style="background-image: url(${promocion.imgBoton});" data-text="${promocion.dataTextContenedorHijo}" class="link-1" title="${promocion.title}" href="${promocion.href}">
+                <a rel="noopener noreferrer" style="background-image: url(${promocion.imgBoton});" data-text="${promocion.dataTextContenedorHijo}" class="link-1" title="${promocion.title}">
                 </a>
             </li>
         `;
--- script/secciones/02-sobre-nosotros/backEndSobreNosotros.js
+++ script/secciones/02-sobre-nosotros/backEndSobreNosotros.js
@@ -9,21 +9,21 @@ const backEndSucursales = [
         mapa: `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3687.020480390165!2d-99.3366!3d19.3055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf8682a38d35d%3A0xd1f0dcade11cd38f!2sC.%20Llano%20Grande%208%2C%20La%20Pila%2C%20Cuajimalpa%20de%20Morelos%2C%2005750%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e1!3m2!1ses!2smx!4v1773848006067!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
         encabezadoPrincipal: "SUCURSAL 1",
         textoPrincipal: "<strong>ipsum dolor</strong> ipsum dolor sit amet consectetur, adipisicing elit.",
-        title: "Ubicación de la sucursal 1.",
+        title: "Ubicación de la sucursal 1",
         href: "#",
     },
     {
         mapa: `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3687.020480390165!2d-99.3366!3d19.3055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf8682a38d35d%3A0xd1f0dcade11cd38f!2sC.%20Llano%20Grande%208%2C%20La%20Pila%2C%20Cuajimalpa%20de%20Morelos%2C%2005750%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e1!3m2!1ses!2smx!4v1773848006067!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
         encabezadoPrincipal: "SUCURSAL 2",
         textoPrincipal: "Lorem ipsum dolor sit amet consectetur, <strong>ipsum dolor</strong>.",
-        title: "Ubicación de la sucursal 2.",
+        title: "Ubicación de la sucursal 2",
         href: "#",
     },
     {
         mapa: `<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3687.020480390165!2d-99.3366!3d19.3055!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cdf8682a38d35d%3A0xd1f0dcade11cd38f!2sC.%20Llano%20Grande%208%2C%20La%20Pila%2C%20Cuajimalpa%20de%20Morelos%2C%2005750%20Ciudad%20de%20M%C3%A9xico%2C%20CDMX!5e1!3m2!1ses!2smx!4v1773848006067!5m2!1ses!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`,
         encabezadoPrincipal: "SUCURSAL 3",
         textoPrincipal: "Lorem <strong>ipsum dolor</strong> sit amet consectetur, adipisicing elit.",
-        title: "Ubicación de la sucursal 3.",
+        title: "Ubicación de la sucursal 3",
         href: "#",
     },
 ];
@@ -46,7 +46,7 @@ const innerHTMLCardsSucursales = (contenedor) => {
                                 ${sucursal.textoPrincipal}
                             </p>
                             <div class="display-flex align-items-center justify-content-center">
-                                <a title="${sucursal.title}" href="${sucursal.href}" class="boton-1 position-relative">
+                                <a rel="noopener noreferrer" target="_blank" title="${sucursal.title}" class="boton-1 position-relative">
                                     Ir a ubicación
                                 </a>
                             </div>
@@ -106,7 +106,7 @@ const innerHTMLCardsBarberos = (contenedor) => {
         contenido +=
             `<div class="card-barbero display-flex flex-direction-column justify-content-center align-items-center">
                 <div class="flex-1">
-                    <img style="animation-duration: ${card.tiempoAnimacion}s;" class="imgen-burbuja" src="${card.imgBarbero}" alt="Foto de barbero.">
+                    <img style="animation-duration: ${card.tiempoAnimacion}s;" class="imgen-burbuja" src="${card.imgBarbero}" alt="Foto del barbero">
                 </div>
                 <h4>
                     ${card.nombreDelBarbero}
@@ -120,18 +120,18 @@ const innerHTMLCardsBarberos = (contenedor) => {
                 </h3>
                 <ul class="display-flex align-items-center justify-content-center">
                     <li class="display-flex align-items-center justify-content-center">
-                        <a class="red-social display-flex align-items-center justify-content-center" title="¡Visítame en Facebook!" href="${card.hrefFacebook}">
-                            <img src="./img/iconos/redes-sociales/facebook-naranja.png" alt="Facebook.">
+                        <a rel="noopener noreferrer" target="_blank" class="red-social display-flex align-items-center justify-content-center" title="Visítame en Facebook">
+                            <img src="./img/iconos/redes-sociales/facebook-naranja.png" alt="Facebook">
                         </a>
                     </li>
                     <li class="display-flex align-items-center justify-content-center">
-                        <a class="red-social display-flex align-items-center justify-content-center" title="¡Visítame en Instagram!" href="${card.hrefInstagram}">
-                            <img src="./img/iconos/redes-sociales/instagram-naranja.png" alt="Instagram.">
+                        <a rel="noopener noreferrer" target="_blank" class="red-social display-flex align-items-center justify-content-center" title="Visítame en Instagram">
+                            <img src="./img/iconos/redes-sociales/instagram-naranja.png" alt="Instagram">
                         </a>
                     </li>
                     <li class="display-flex align-items-center justify-content-center">
-                        <a class="red-social display-flex align-items-center justify-content-center" title="¡Visítame en TikTok!" href="${card.hrefTikTok}">
-                            <img src="./img/iconos/redes-sociales/tiktok-naranja.png" alt="TikTok.">
+                        <a rel="noopener noreferrer" target="_blank" class="red-social display-flex align-items-center justify-content-center" title="Visítame en TikTok">
+                            <img src="./img/iconos/redes-sociales/tiktok-naranja.png" alt="TikTok">
                         </a>
                     </li>
                 </ul>
